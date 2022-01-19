310066f6d5af87a8f5ad663ee4c940ebf6b0c5b7
Kirill Kouzoubov <kirill888@gmail.com>
2022-01-19 15:08:40 +1100

    Add odc.geo.xr
    For now it's just importing things from _xr_interop, but it might be
    expanded to do some import time logic for hooking in xarray integrations
    so probably best to keep this extra indirection rather than just
    renaming private modules into public.

---
+++ odc/geo/xr.py
@@ -0,0 +1,30 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2020 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""
+Xarray Intergration.
+
+Importing this module enables `.odc.` extension on :py:class:`xarray.DataArray` and
+:py:class:`xarray.Dataset` objects.
+
+For more details see: :py:class:`~odc.geo.xr.ODCExtension` and
+:py:class:`~odc.geo.xr.ODCExtensionDs`.
+"""
+from ._xr_interop import (
+    ODCExtension,
+    ODCExtensionDs,
+    assign_crs,
+    register_geobox,
+    spatial_dims,
+    xr_coords,
+)
+
+__all__ = (
+    "ODCExtension",
+    "ODCExtensionDs",
+    "assign_crs",
+    "register_geobox",
+    "spatial_dims",
+    "xr_coords",
+)
