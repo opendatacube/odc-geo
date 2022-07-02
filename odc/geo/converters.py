e8c0e97930272ba54827a3a69e0b772fe9ac3417
Kirill Kouzoubov <kirill888@gmail.com>
2022-07-02 14:23:55 +1000

    Tools for extracting GCPs from rasterio reader

--- odc/geo/converters.py
+++ odc/geo/converters.py
@@ -2,9 +2,11 @@
 Interop with other geometry libraries.
 """
 
-from typing import List
+from typing import List, Tuple
 
-from .geom import Geometry
+from .crs import CRS, MaybeCRS, norm_crs
+from .geom import Geometry, point
+from .types import XY, xy_
 
 
 def from_geopandas(series) -> List[Geometry]:
@@ -18,3 +20,38 @@ def from_geopandas(series) -> List[Geometry]:
         return []
 
     return [Geometry(g, crs) for g in gg]
+
+
+def extract_gcps_raw(src) -> Tuple[List[XY[float]], List[XY[float]], CRS]:
+    """
+    Extract Ground Control points from :py:class:`rasterio.DatasetReader`.
+
+    :returns: ``[pixel coors], [world coords], CRS``
+    """
+    pts, gcp_crs = src.gcps
+    if len(pts) == 0:
+        raise ValueError(f"Could not access GCP on {src}")
+
+    pix_pts = [xy_(pt.col, pt.row) for pt in pts]
+    wld_pts = [xy_(pt.x, pt.y) for pt in pts]
+
+    return pix_pts, wld_pts, norm_crs(gcp_crs)
+
+
+def extract_gcps(
+    src,
+    output_crs: MaybeCRS = None,
+) -> Tuple[List[XY[float]], List[Geometry]]:
+    """
+    Extract Ground Control points from :py:class:`rasterio.DatasetReader`.
+
+    :returns: ``[pixel coors], [world coords]``
+    """
+    pix, wld, gcp_crs = extract_gcps_raw(src)
+    output_crs = norm_crs(output_crs)
+
+    wld_pts = (point(pt.x, pt.y, gcp_crs) for pt in wld)
+    if output_crs is not None and gcp_crs != output_crs:
+        wld_pts = (pt.to_crs(output_crs) for pt in wld_pts)
+
+    return pix, list(wld_pts)
--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,12 +1,18 @@
-import geopandas
-import geopandas.datasets
+# pylint: disable=wrong-import-position
+from pathlib import Path
+
+import pytest
+
+rasterio = pytest.importorskip("rasterio")
+gpd = pytest.importorskip("geopandas")
+gpd_datasets = pytest.importorskip("geopandas.datasets")
 
 from odc.geo._interop import have
-from odc.geo.converters import from_geopandas
+from odc.geo.converters import extract_gcps, from_geopandas
 
 
 def test_from_geopandas():
-    df = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
+    df = gpd.read_file(gpd_datasets.get_path("naturalearth_lowres"))
     gg = from_geopandas(df)
     assert isinstance(gg, list)
     assert len(gg) == len(df)
@@ -31,3 +37,19 @@ def test_have():
     assert isinstance(have.datacube, bool)
     assert have.rasterio is True
     assert have.xarray is True
+
+
+def test_extract_gcps(data_dir: Path):
+    with rasterio.open(data_dir / "au-gcp.tif") as src:
+        assert src.gcps != ([], None)
+        pix1, wld_native = extract_gcps(src)
+        pix2, wld_3857 = extract_gcps(src, "epsg:3857")
+
+    assert pix1 == pix2
+    assert all(pt.crs == "epsg:4326" for pt in wld_native)
+    assert all(pt.crs == "epsg:3857" for pt in wld_3857)
+
+    with rasterio.open(data_dir / "au-3577.tif") as src:
+        assert src.gcps == ([], None)
+        with pytest.raises(ValueError):
+            _ = extract_gcps(src)
