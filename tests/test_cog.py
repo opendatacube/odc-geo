acda7f7289b36c21e06a04d857c78b5f560cbc9e
Kirill Kouzoubov <kirill888@gmail.com>
2023-08-28 15:02:32 +1000

    Add internal helper method for cog writing
    Computes number of overviews and padding to have safe geo-referencing of
    overviews. Overview pixel for each level should cover native pixels
    without sub-pixel cropping.

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,3 +1,8 @@
+from typing import Optional, Tuple
+
+import pytest
+
+from odc.geo._cog import _compute_cog_spec, _num_overviews
 from odc.geo.gridspec import GridSpec
 from odc.geo.xr import xr_zeros
 
@@ -37,3 +42,48 @@ def test_write_cog_ovr():
 
     img_bytes2 = img.odc.write_cog(":mem:", blocksize=32, overviews=ovrs)
     assert len(img_bytes) == len(img_bytes2)
+
+
+@pytest.mark.parametrize(
+    ["block", "dim", "n_expect"],
+    [
+        (2**5, 2**10, 5),
+        (2**5, 2**10 - 1, 5),
+        (2**5, 2**10 + 1, 5),
+        (256, 78, 0),
+        (1024, 1_000_000, -1),
+        (512, 3_040_000, -1),
+    ],
+)
+def test_num_overviews(block: int, dim: int, n_expect: int):
+    if n_expect >= 0:
+        assert _num_overviews(block, dim) == n_expect
+    else:
+        n = _num_overviews(block, dim)
+        assert dim // (2**n) <= block
+
+
+@pytest.mark.parametrize(
+    ("shape", "tshape", "max_pad"),
+    [
+        [(1024, 2048), (256, 128), None],
+    ],
+)
+def test_cog_spec(
+    shape: Tuple[int, int],
+    tshape: Tuple[int, int],
+    max_pad: Optional[int],
+):
+    _shape, _tshape, nlevels = _compute_cog_spec(shape, tshape, max_pad=max_pad)
+    assert _shape[0] >= shape[0]
+    assert _shape[1] >= shape[1]
+    assert _tshape[0] >= tshape[0]
+    assert _tshape[1] >= tshape[1]
+    assert _tshape[0] % 16 == 0
+    assert _tshape[1] % 16 == 0
+
+    assert max(_shape) // (2**nlevels) <= max(tshape)
+
+    if max_pad is not None:
+        assert _shape[0] - shape[0] <= max_pad
+        assert _shape[1] - shape[1] <= max_pad
