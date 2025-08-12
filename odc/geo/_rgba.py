46c9f16abb7ee4749759a6ddbd93084f34d51561
Kirill Kouzoubov <kirill888@gmail.com>
2025-08-12 10:31:46 +0000

    maint: ignore mypy false positives

--- odc/geo/_rgba.py
+++ odc/geo/_rgba.py
@@ -157,7 +157,7 @@ def to_rgba(
         data = _np_to_rgba(r, g, b, nodata, vmin, vmax)
 
     coords = dict(_b.coords.items())
-    coords.update(band=xr.DataArray(data=["r", "g", "b", "a"], dims=("band",)))
+    coords.update(band=xr.DataArray(data=["r", "g", "b", "a"], dims=("band",)))  # type: ignore
 
     rgba = xr.DataArray(data, coords=coords, dims=dims)
     return rgba
