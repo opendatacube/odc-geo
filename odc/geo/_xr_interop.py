b5460c76bbe2a3903917b2dd81435b778c20ab25
andrewhicks <andrew.hicks@ga.gov.au>
2026-05-15 00:17:37 +0000

    Fix linting/formatting

--- odc/geo/_xr_interop.py
+++ odc/geo/_xr_interop.py
@@ -887,6 +887,7 @@ def _xr_reproject_ds(
     dtype=None,
     **kw,
 ) -> xarray.Dataset:
+    # pylint: disable=too-many-locals
     assert isinstance(src, xarray.Dataset)
 
     if len(src.dims) == 1:
@@ -929,17 +930,17 @@ def _xr_reproject_ds(
             **kw,
         )
 
-    data_vars = {
-        name: _maybe_reproject(da)
-        for name, da in src.data_vars.items()
-    }
+    data_vars = {name: _maybe_reproject(da) for name, da in src.data_vars.items()}
+    # Copy over non spatial-attrs and coords
     attrs = {name: value for name, value in src.attrs if name not in SPATIAL_ATTRIBUTES}
     dst = xarray.Dataset(data_vars, attrs=attrs)
+    src_spatial_dims: tuple = spatial_dims(src) or tuple()
+    src_crs_coords = _locate_crs_coords(src)
     for src_coord_name, src_coord in src.coords.items():
         if (
             src_coord_name not in dst.coords
-            and not set(src_coord.dims).issubset(src.odc.spatial_dims)
-            and not src_coord_name == src.odc.crs_coord
+            and not set(src_coord.dims).issubset(src_spatial_dims)
+            and src_coord not in src_crs_coords
         ):
             dst.coords[src_coord_name] = src_coord
     return dst
